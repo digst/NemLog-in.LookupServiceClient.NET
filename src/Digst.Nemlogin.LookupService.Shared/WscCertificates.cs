b4be4f42c0928bb162344aba6a25fd611d7c90fc
Thomas Nymand <thomas@nymand.net>
2023-10-31 19:27:52 +0100

    A bit of cleanup, added README.md

--- src/Digst.Nemlogin.LookupService.Shared/WscCertificates.cs
+++ src/Digst.Nemlogin.LookupService.Shared/WscCertificates.cs
@@ -49,7 +49,7 @@ namespace Digst.Nemlogin.LookupService.Shared
             return LoadCertificateFrom(certificatePath, "Test1234");
         }
 
-        private X509Certificate2 LoadCertificateFrom(string filePath, string password = null)
+        private X509Certificate2 LoadCertificateFrom(string filePath, string password)
         {
             try
             {
